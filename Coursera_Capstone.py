87fec22f52c25a0ad81021f29d72f79a8944a7e8
rmaldonadob <74161259+rmaldonadob@users.noreply.github.com>
2020-11-08 20:27:17 -0500

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone for Data Science
+
+# ***Renzo Maldonado***
+
+# Welcome to my notebook for the final project of the Data Science specialization by IBM and Coursera...
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[8]:
+
+
+print("Hello Capstone Project Course!")
+
